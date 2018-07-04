d9c67ebc5c5df42aebe62b7a3120cd09d1c12619
markspencertan <msgtan@gmail.com>
2018-07-04 03:24:49 -0700

    added Navigation component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
+import Navigation from './Navigation';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+
+ReactDOM.render(<Navigation />, document.getElementById('nav'));
+ReactDOM.render(<App />, document.getElementById('content'));
+
 registerServiceWorker();
